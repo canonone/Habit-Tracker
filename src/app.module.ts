713c6a3c90bb96fcf88fc55a7eca1a77ef53cff3
christian umoh <umohchristian001@gmail.com>
2025-06-15 23:29:56 +0100

    chore: added the config module to the imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,9 @@ import Task from './modules/task/entities/task.entity';
 import User from './modules/user/entities/user.entity';
 import UserModule from './modules/user/user.module';
 import TaskModule from './modules/task/task.module';
+import AuthModule from './modules/auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+import authConfig from './config/auth.config';
 
 @Module({
   imports: [
@@ -13,8 +16,13 @@ import TaskModule from './modules/task/task.module';
       entities: [Task, User],
       synchronize: true,
     }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [authConfig],
+    }),
     UserModule,
     TaskModule,
+    AuthModule,
   ],
 })
 export default class AppModule {}
