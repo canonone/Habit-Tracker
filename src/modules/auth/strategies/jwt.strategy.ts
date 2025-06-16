de2cf79537a238c8f7cc9efe505d1d3e4db68cd0
christian umoh <umohchristian001@gmail.com>
2025-06-16 01:35:29 +0100

    chore: fully implemented the jwt strategy

--- src/modules/auth/strategies/jwt.strategy.ts
+++ src/modules/auth/strategies/jwt.strategy.ts
@@ -1,4 +1,23 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import Strategy from 'passport-jwt';
+import * as ExtractJwt from 'passport-jwt/lib/extract_jwt';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
-export default class JwtStrategy{}
+export default class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(private readonly configService: ConfigService) {
+    const secretKey: string =
+      configService.get<string>('JWT_SECRET') ?? 'defaultSecretkey';
+
+    super({
+      jwtfromRequest: ExtractJwt.FromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: secretKey,
+    });
+  }
+
+  validate(payload: { sub: string; email: string }) {
+    return { userId: payload.sub, email: payload.email };
+  }
+}
