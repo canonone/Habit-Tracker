c93727b380b86d0ffcfb3b71799861e1a0c5cdd1
christian umoh <umohchristian001@gmail.com>
2025-06-27 13:16:48 +0100

    chores: added the provider property to select the auth method

--- src/modules/user/entities/user.entity.ts
+++ src/modules/user/entities/user.entity.ts
@@ -1,15 +1,31 @@
-import { Entity, Column, OneToMany } from 'typeorm';
+import { Entity, Column, OneToMany, BeforeInsert } from 'typeorm';
 import BaseEntity from 'src/utils/base.entity';
 import Task from 'src/modules/task/entities/task.entity';
 
+export enum AuthProvider {
+  LOCAL = 'local',
+  GOOGLE = 'google',
+}
+
 @Entity()
 export default class User extends BaseEntity {
+  @Column({ nullable: true })
+  fullName: string;
+
   @Column()
   email: string;
 
   @Column()
   password: string;
 
-  @OneToMany(()=>Task, tasks=>tasks.user)
-  tasks: Task[]
+  @OneToMany(() => Task, (tasks) => tasks.user)
+  tasks: Task[];
+
+  @Column({
+    type: 'enum',
+    enum: AuthProvider,
+    default: AuthProvider.LOCAL,
+    nullable: false,
+  })
+  provier: AuthProvider;
 }
