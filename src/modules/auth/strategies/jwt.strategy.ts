4c8f8f5d5fb6da5a142287f44758c0e903e20e17
christian umoh <umohchristian001@gmail.com>
2025-06-27 13:23:28 +0100

    feat: implemented the strategy for handling jwt tokens

--- src/modules/auth/strategies/jwt.strategy.ts
+++ src/modules/auth/strategies/jwt.strategy.ts
@@ -1,23 +1,26 @@
 import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
-import Strategy from 'passport-jwt';
+import { Strategy } from 'passport-jwt';
 import * as ExtractJwt from 'passport-jwt/lib/extract_jwt';
 import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export default class JwtStrategy extends PassportStrategy(Strategy) {
   constructor(private readonly configService: ConfigService) {
-    const secretKey: string =
-      configService.get<string>('JWT_SECRET') ?? 'defaultSecretkey';
+    const secret: string =
+      configService.get<string>('JWT_SECRET') ?? 'defaultSecretKey';
 
     super({
-      jwtfromRequest: ExtractJwt.FromAuthHeaderAsBearerToken(),
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: secretKey,
+      secretOrKey: secret,
     });
   }
 
-  validate(payload: { sub: string; email: string }) {
-    return { userId: payload.sub, email: payload.email };
+  async validate(payload: { sub: string; email: string }) {
+    return {
+      id: payload.sub,
+      email: payload.email,
+    };
   }
 }
