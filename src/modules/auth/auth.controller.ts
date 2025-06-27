d31158527a6db69664ca8ad1967a77139759c806
christian umoh <umohchristian001@gmail.com>
2025-06-27 13:18:55 +0100

    chores: added the google auth routes

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,7 +1,9 @@
-import { Controller, Post, Get, Body, Param } from '@nestjs/common';
+import { Controller, Post, Get, Body, Param, Req } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import CreateUserDto from '../user/dtos/create-user.dto';
 import LoginDto from '../user/dtos/user-login.dto';
+import { AuthGuard } from '@nestjs/passport';
+import { UseGuards } from '@nestjs/common';
 
 @Controller('auth')
 export class AuthController {
@@ -9,7 +11,7 @@ export class AuthController {
 
   @Post('/register')
   async register(@Body() userDto: CreateUserDto) {
-    const user = await this.authService.createUser(userDto);
+    const user = await this.authService.register(userDto);
     const { password, ...result } = user;
     return {
       status: 'successful',
@@ -21,5 +23,15 @@ export class AuthController {
   @Post('/login')
   async userLogin(@Body() loginDto: LoginDto) {
     return this.authService.loginUser(loginDto);
+  }
+
+  @Get('google')
+  @UseGuards(AuthGuard('google'))
+  async googleAuth(@Req() req) {}
+
+  @Get('google/redirect')
+  @UseGuards(AuthGuard('google'))
+  googleAuthRedirect(@Req() req) {
+    return req.user;
   }
 }
